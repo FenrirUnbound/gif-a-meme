ac0211412e65e2fa831cafdc38e3ccd3e576a9d4
Darren M <aeneascorrupt@gmail.com>
2019-07-17 00:36:42 -0700

    enable option for querying video info

--- magic.py
+++ magic.py
@@ -9,6 +9,7 @@ from operations.gif_it import GenerateGifIt
 
 DEFAULT_VIDEO = 'https://www.youtube.com/watch?v=CGOPPzh8TJ4'
 SHARE_DIR = '/usr/src/share'
+VIDEO_QUALITY = 'context_video_quality'
 
 def _generate_config(overrides={}):
     temp_dir = tempfile.mkdtemp()
@@ -28,14 +29,17 @@ def _generate_config(overrides={}):
         'source_path': source_path,
         'subtitle_path': '/usr/src/share/subtitles.yaml',
         'temp_dir': temp_dir,
-        'video_quality': 135,
+        'video_quality': overrides['video_quality'],
         'video_url': overrides['video_url'] if 'video_url' in overrides else DEFAULT_VIDEO
     }
 
 @click.group()
+@click.option('--video-quality', '-q', default=135, type=int)
 @click.pass_context
-def cli(ctx):
-    pass
+def cli(ctx, video_quality):
+    ctx.ensure_object(dict)
+
+    ctx.obj[VIDEO_QUALITY] = video_quality
 
 @cli.command()
 @click.option('--output', '-o', default='/usr/src/share/results.gif', type=str)
@@ -45,7 +49,8 @@ def run(ctx, output, input_video):
     """Runs the entire recipe: download, conversion, subtitle, save"""
     overrides = {
         'input_video': input_video,
-        'output_path': output
+        'output_path': output,
+        'video_quality': ctx.obj[VIDEO_QUALITY]
     }
     config = _generate_config(overrides=overrides)
     
@@ -58,20 +63,27 @@ def run(ctx, output, input_video):
     sys.exit(exit_code)
 
 @cli.command()
-@click.pass_context
+@click.option('--info', is_flag=True)
 @click.argument('url')
-def download(ctx, url):
+@click.pass_context
+def download(ctx, info, url):
     """Download the desired video"""
     config = _generate_config({
         'output_path': '/usr/src/share/results.gif',  # todo: remove this
+        'video_quality': ctx.obj[VIDEO_QUALITY],
         'video_save_path': os.path.join(SHARE_DIR, 'video.mp4'),
         'video_url': url
     })
+    cmd = None
+
     generator = GenerateGifIt()
-    cmd = generator.get_downloader()
+    if info:
+        cmd = generator.get_video_options()
+    else:
+        cmd = generator.get_downloader()
 
     exit_code = cmd.exec(config=config)
     sys.exit(exit_code)
 
 if __name__ == '__main__':
-    cli()
+    cli(obj={})
